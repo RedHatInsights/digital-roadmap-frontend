e71e69cfdf265cb8671116ef8a32bd085b965160
Andrea Waltlova <awaltlov@redhat.com>
2024-10-21 14:25:09 +0200

    Fix lint and update tabs content
    Signed-off-by: Andrea Waltlova <awaltlov@redhat.com>

--- src/Routes/LandingPage/LandingPage.tsx
+++ src/Routes/LandingPage/LandingPage.tsx
@@ -1,23 +1,19 @@
 import React, { Suspense, lazy, useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
-
-import {
-  Spinner,
-} from '@patternfly/react-core';
+import { Spinner } from '@patternfly/react-core';
 import {
   PageHeader,
   PageHeaderTitle,
 } from '@redhat-cloud-services/frontend-components/PageHeader';
 
 import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
-import { Tab, Tabs, TabTitleText } from '@patternfly/react-core';
+import { Tab, TabTitleText, Tabs } from '@patternfly/react-core';
 
 import './landing-page.scss';
 
-const ReleasedTab = lazy(
-  () => import('../../Components/Released/released')
-);
+const ReleasedTab = lazy(() => import('../../Components/Released/released'));
+const UpcomingTab = lazy(() => import('../../Components/Upcoming/upcoming'));
 
 /**
  * A smart component that handles all the api calls and data needed by the dumb components.
@@ -35,7 +31,8 @@ const LandingPage = () => {
   const tabsPath = ['released', 'upcoming'];
 
   const tabPath = pathname.split('/').pop() || 'released';
-  const initialActiveTabKey = tabsPath.indexOf(tabPath) >= 0 ? tabsPath.indexOf(tabPath) : 0;
+  const initialActiveTabKey =
+    tabsPath.indexOf(tabPath) >= 0 ? tabsPath.indexOf(tabPath) : 0;
   const [activeTabKey, setActiveTabKey] = useState(initialActiveTabKey);
 
   useEffect(() => {
@@ -46,7 +43,10 @@ const LandingPage = () => {
     setActiveTabKey(initialActiveTabKey);
   }, [pathname]);
 
-  const handleTabClick = (_event: React.MouseEvent<HTMLElement, MouseEvent>, tabIndex: number | string) => {
+  const handleTabClick = (
+    _event: React.MouseEvent<HTMLElement, MouseEvent>,
+    tabIndex: number | string
+  ) => {
     if (typeof tabIndex === 'string') tabIndex = Number(tabIndex);
     const tabPath = tabsPath[tabIndex];
     if (tabPath !== undefined) {
@@ -59,9 +59,17 @@ const LandingPage = () => {
     <React.Fragment>
       <PageHeader>
         <PageHeaderTitle title="Digital Roadmap" />
-        <p>Provides tailored forward-looking roadmap information and tailored information on how RHEL minor and major releases will affect the customers environment</p>
+        <p>
+          Provides tailored forward-looking roadmap information and tailored
+          information on how RHEL minor and major releases will affect the
+          customers environment
+        </p>
       </PageHeader>
-      <Tabs className="pf-c-tabs pf-c-page-header pf-c-table" activeKey={activeTabKey} onSelect={handleTabClick}>
+      <Tabs
+        className="pf-c-tabs pf-c-page-header pf-c-table"
+        activeKey={activeTabKey}
+        onSelect={handleTabClick}
+      >
         <Tab eventKey={0} title={<TabTitleText>Released</TabTitleText>}>
           <section className="pf-l-page__main-section pf-c-page__main-section">
             <Suspense fallback={<Spinner />}>
@@ -71,7 +79,9 @@ const LandingPage = () => {
         </Tab>
         <Tab eventKey={1} title={<TabTitleText>Upcoming</TabTitleText>}>
           <section className="pf-l-page__main-section pf-c-page__main-section">
-            TODO: Upcoming
+            <Suspense fallback={<Spinner />}>
+              <UpcomingTab />
+            </Suspense>
           </section>
         </Tab>
       </Tabs>
