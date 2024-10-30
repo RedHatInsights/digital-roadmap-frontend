40191bb18713bd62ec0d7d3e6441fa7d2f15aea7
Andrea Waltlova <awaltlov@redhat.com>
2024-10-30 17:49:25 +0100

    Add dummy components to Released tab
    Added dummy dropdown
    Added dummy togglegroup
    Added dummy sidebar with content
    
    Signed-off-by: Andrea Waltlova <awaltlov@redhat.com>

---
+++ src/Components/ReleasedViewToolbar/CustomDropdown.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+  MenuToggle,
+  Select,
+  SelectList,
+  SelectOption,
+} from '@patternfly/react-core';
+import BellIcon from '@patternfly/react-icons/dist/esm/icons/bell-icon';
+import FilterIcon from '@patternfly/react-icons/dist/esm/icons/filter-icon';
+
+export const SelectOptionVariations: React.FunctionComponent = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState<string>('Select a value');
+
+  const onToggleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const onSelect = (
+    _event: React.MouseEvent<Element, MouseEvent> | undefined,
+    value: string | number | undefined
+  ) => {
+    setSelected(value as string);
+    setIsOpen(false);
+  };
+
+  const toggle = (toggleRef: React.Ref<HTMLDivElement | HTMLButtonElement>) => (
+    <MenuToggle
+      icon={<FilterIcon />}
+      ref={toggleRef}
+      onClick={onToggleClick}
+      isExpanded={isOpen}
+      style={
+        {
+          width: '200px',
+        } as React.CSSProperties
+      }
+    >
+      {selected}
+    </MenuToggle>
+  );
+
+  return (
+    <Select
+      id="option-variations-select"
+      isOpen={isOpen}
+      selected={selected}
+      onSelect={onSelect}
+      //   onOpenChange={(isOpen) => setIsOpen(isOpen)}
+      toggle={toggle}
+      shouldFocusToggleOnSelect
+    >
+      <SelectList>
+        <SelectOption value="Basic option">Basic option</SelectOption>
+        <SelectOption
+          value="Option with description"
+          description="This is a description"
+        >
+          Option with description
+        </SelectOption>
+        <SelectOption
+          to="#"
+          // Prevent default clicking functionality for example purposes only
+          onClick={(event) => event.preventDefault()}
+          value="Option with link"
+          isExternalLink
+        >
+          Option with link
+        </SelectOption>
+        <SelectOption value="Option with icon" icon={<BellIcon />}>
+          Option with icon
+        </SelectOption>
+        <SelectOption value="Disabled option" isDisabled>
+          Disabled option
+        </SelectOption>
+        <SelectOption value="See Menu for additional variations!">
+          See Menu for additional variations!
+        </SelectOption>
+      </SelectList>
+    </Select>
+  );
+};
+
+export default SelectOptionVariations;
