647b0dcb550e6101115ef35957b73a2134335b00
Andrea Waltlova <awaltlov@redhat.com>
2024-11-07 14:19:21 +0100

    Add buttons making requests to backend ith database
    Signed-off-by: Andrea Waltlova <awaltlov@redhat.com>

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,5 @@
-export const DIGITAL_ROADMAP_API = '/api/digital-roadmap/v1';
+export const DR_API = '/api/digital-roadmap/v1';
+export const DR_RELEASE_NOTES = '/release-notes';
+
+export const INVENTORY_API_ROOT = '/api/inventory/v1';
+export const INVENTORY_HOSTS_ROOT = '/hosts';
