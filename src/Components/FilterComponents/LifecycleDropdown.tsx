1b7c6461eea6461a6eaa809243d56dc7dd282ad0
Rebecca Alpert <ralpert@redhat.com>
2025-02-05 13:16:42 -0500

    feat(LifecycleTable): Tie tables into one controlled by dropdown (#20)
    Migrated logic from both tables into 1 shared table that keys off of data type. The dropdown should control what data is visible. This change does not have perfect filtering - will fix in follow-on.
    
    Co-authored-by: Adam Hosek <ahosek@redhat.com>

--- src/Components/FilterComponents/LifecycleDropdown.tsx
+++ src/Components/FilterComponents/LifecycleDropdown.tsx
@@ -1,19 +1,29 @@
 import React from 'react';
-import { Dropdown, DropdownItem, DropdownList, Divider, MenuToggle, Select, SelectList, SelectOption} from '@patternfly/react-core';
+import { MenuToggle, Select, SelectList, SelectOption } from '@patternfly/react-core';
 
-export const LifecycleDropdown: React.FunctionComponent = () => {
+interface LifecycleDropdownProps {
+  currentValue: string;
+  setCurrentValue: (value: string) => void;
+  onDropdownSelect: (value: string) => void;
+}
+
+export const LifecycleDropdown: React.FunctionComponent<LifecycleDropdownProps> = ({
+  currentValue,
+  setCurrentValue,
+  onDropdownSelect,
+}: LifecycleDropdownProps) => {
   const [isOpen, setIsOpen] = React.useState(false);
-  const [selected, setSelected] = React.useState<string>('RHEL 9 Application Streams');
 
   const onToggleClick = () => {
     setIsOpen(!isOpen);
   };
 
   const onSelect = (_event: React.MouseEvent<Element, MouseEvent> | undefined, value: string | number | undefined) => {
-    // eslint-disable-next-line no-console
-    console.log('selected', value);
-    setSelected(value as string);
     setIsOpen(false);
+    if (typeof value === 'string') {
+      setCurrentValue(value);
+      onDropdownSelect(value);
+    }
   };
 
   const toggle = (toggleRef: React.Ref<HTMLDivElement | HTMLButtonElement>) => (
@@ -27,26 +37,26 @@ export const LifecycleDropdown: React.FunctionComponent = () => {
         } as React.CSSProperties
       }
     >
-      {selected}
+      {currentValue}
     </MenuToggle>
   );
 
   return (
     <Select
-    id="option-variations-select"
-    isOpen={isOpen}
-    selected={selected}
-    onSelect={onSelect}
-    //   onOpenChange={(isOpen) => setIsOpen(isOpen)}
-    toggle={toggle}
-    shouldFocusToggleOnSelect
+      id="option-variations-select"
+      isOpen={isOpen}
+      selected={currentValue}
+      onSelect={onSelect}
+      //   onOpenChange={(isOpen) => setIsOpen(isOpen)}
+      toggle={toggle}
+      shouldFocusToggleOnSelect
     >
       <SelectList>
         <SelectOption value="RHEL 9 Application Streams">RHEL 9 Application Streams</SelectOption>
-        <SelectOption value="Red Hat Enterprise Linux" >Red Hat Enterprise Linux</SelectOption>
+        <SelectOption value="Red Hat Enterprise Linux">Red Hat Enterprise Linux</SelectOption>
       </SelectList>
     </Select>
-);
+  );
 };
 
 export default LifecycleDropdown;
