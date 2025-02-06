5c33c16410592f342c5d918a9f479ad7858ee717
Preston Watson <prwatson@redhat.com>
2025-02-06 16:39:39 -0500

    Added files

--- src/Components/LifecycleChart/LifecycleChart.tsx
+++ src/Components/LifecycleChart/LifecycleChart.tsx
@@ -25,19 +25,19 @@ const LifecycleChart: React.FC<LifecycleChartProps> = ({ lifecycleData }: Lifecy
     return 'lifecycle';
   };
 
-  const lifecycleChartData = [
-  [{ x: 'RHEL 8.3', y0: new Date('2023-01'), y: new Date('2024-06'), packageType: 'Retired' }],
-  [
-    {
-      x: 'RHEL 8.7',
-      y0: new Date('2023-01'),
-      y: new Date('2025-10'),
-      packageType: 'Support ends within 6 months',
-    },
-  ],
-  [{ x: 'RHEL 9.0', y0: new Date('2024-08'), y: new Date('2025-06'), packageType: 'Not installed' }],
-  [{ x: 'RHEL 9.1', y0: new Date('2023-01'), y: new Date('2027-10'), packageType: 'Supported' }],
-  ];
+  // const lifecycleChartData = [
+  // [{ x: 'RHEL 8.3', y0: new Date('2023-01'), y: new Date('2024-06'), packageType: 'Retired' }],
+  // [
+  //   {
+  //     x: 'RHEL 8.7',
+  //     y0: new Date('2023-01'),
+  //     y: new Date('2025-10'),
+  //     packageType: 'Support ends within 6 months',
+  //   },
+  // ],
+  // [{ x: 'RHEL 9.0', y0: new Date('2024-08'), y: new Date('2025-06'), packageType: 'Not installed' }],
+  // [{ x: 'RHEL 9.1', y0: new Date('2023-01'), y: new Date('2027-10'), packageType: 'Supported' }],
+  // ];
 
   const dataType = checkDataType(lifecycleData);
   const updatedLifecycleData : any[][] = []
---
+++ src/types/SystemLifecycleChanges.ts
@@ -0,0 +1,10 @@
+export type SystemLifecycleChanges = {
+  name: string;
+  release: string;
+  major: number;
+  minor: number;
+  release_date: Date;
+  retirement_date: Date;
+  systems: number;
+  lifecycle_type: string;
+};
