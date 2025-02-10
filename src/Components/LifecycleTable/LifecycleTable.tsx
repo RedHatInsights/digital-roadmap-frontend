581dea3245f8e19a543dc4a290ae0b0a3f46b38c
Rebecca Alpert <ralpert@redhat.com>
2025-02-10 12:21:11 -0500

    feat(NameFilter): Filter stream data correctly (#25)
    Lift up data structure into Lifecycle tab so we can filter based on name displayed in table.

--- src/Components/LifecycleTable/LifecycleTable.tsx
+++ src/Components/LifecycleTable/LifecycleTable.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Table, Tbody, Td, Th, ThProps, Thead, Tr } from '@patternfly/react-table';
 import Moment from 'moment';
-import { AppLifecycleChanges } from '../../types/AppLifecycleChanges';
-import { SystemLifecycleChanges} from '../../types/SystemLifecycleChanges';
-
+import { SystemLifecycleChanges } from '../../types/SystemLifecycleChanges';
+import { Stream } from '../../types/Stream';
 interface LifecycleTableProps {
-  data: AppLifecycleChanges[] | SystemLifecycleChanges[];
+  data: Stream[] | SystemLifecycleChanges[];
+  type: 'streams' | 'rhel';
 }
 
 const LIFECYCLE_COLUMN_NAMES = {
@@ -26,24 +26,13 @@ const APP_LIFECYCLE_COLUMN_NAMES = {
 
 const DEFAULT_ARIA_LABEL = 'Table displaying lifecycle information';
 
-export const LifecycleTable: React.FunctionComponent<LifecycleTableProps> = ({ data }: LifecycleTableProps) => {
+export const LifecycleTable: React.FunctionComponent<LifecycleTableProps> = ({ data, type }: LifecycleTableProps) => {
   // Index of the currently sorted column
   // Note: if you intend to make columns reorderable, you may instead want to use a non-numeric key
   // as the identifier of the sorted column. See the "Compound expandable" example.
   const [activeSortIndex, setActiveSortIndex] = React.useState<number | undefined>(undefined);
   // Sort direction of the currently sorted column
   const [activeSortDirection, setActiveSortDirection] = React.useState<'asc' | 'desc' | undefined>(undefined);
-  const checkDataType = (data: AppLifecycleChanges[] | SystemLifecycleChanges[]) => {
-    if (!data || data.length === 0) {
-      return '';
-    }
-    if ('module_name' in data[0]) {
-      return 'appLifecycle';
-    }
-    return 'lifecycle';
-  };
-
-  const dataType = checkDataType(data);
 
   // Since OnSort specifies sorted columns by index, we need sortable values for our object by column index.
   // This example is trivial since our data objects just contain strings, but if the data was more complex
@@ -66,26 +55,27 @@ export const LifecycleTable: React.FunctionComponent<LifecycleTableProps> = ({ d
     columnIndex,
   });
 
-  const renderAppLifecycleData = (data: AppLifecycleChanges[]) =>
-    data
-      .flatMap((repo) => repo.streams)
-      .map((stream) => (
+  const formatDate = (date: string) => {
+    if (date === 'Unknown') {
+      return 'Not available';
+    }
+    return Moment(date).format('MMM YYYY');
+  };
+
+  const renderAppLifecycleData = (data: Stream[]) =>
+    data.map((stream) => {
+      return (
         <Tr key={`${stream.name}-${stream.stream}-${stream.start_date}-${stream.end_date}`}>
           <Td dataLabel={APP_LIFECYCLE_COLUMN_NAMES.name}>
             {stream.name} {stream.stream}
           </Td>
-          <Td dataLabel={APP_LIFECYCLE_COLUMN_NAMES.release}>
-            {
-              data.filter((appLifecycleChanges) =>
-                appLifecycleChanges.streams.some((str) => str.context === stream.context)
-              )[0].rhel_major_version
-            }
-          </Td>
-          <Td dataLabel={APP_LIFECYCLE_COLUMN_NAMES.release_date}>{Moment(stream.start_date).format('MMM YYYY')}</Td>
-          <Td dataLabel={APP_LIFECYCLE_COLUMN_NAMES.retirement_date}>{Moment(stream.end_date).format('MMM YYYY')}</Td>
+          <Td dataLabel={APP_LIFECYCLE_COLUMN_NAMES.release}>{stream.rhel_major_version}</Td>
+          <Td dataLabel={APP_LIFECYCLE_COLUMN_NAMES.release_date}>{formatDate(stream.start_date)}</Td>
+          <Td dataLabel={APP_LIFECYCLE_COLUMN_NAMES.retirement_date}>{formatDate(stream.end_date)}</Td>
           <Td dataLabel={APP_LIFECYCLE_COLUMN_NAMES.systems}>N/A</Td>
         </Tr>
-      ));
+      );
+    });
 
   const renderSystemLifecycleData = (data: SystemLifecycleChanges[]) => {
     // Note that we perform the sort as part of the component's render logic and not in onSort.
@@ -126,11 +116,8 @@ export const LifecycleTable: React.FunctionComponent<LifecycleTableProps> = ({ d
   };
 
   const renderHeaders = () => {
-    if (!dataType) {
-      return;
-    }
-    switch (dataType) {
-      case 'appLifecycle':
+    switch (type) {
+      case 'streams':
         return (
           <Tr key={LIFECYCLE_COLUMN_NAMES.name}>
             <Th sort={getSortParams(0)}>{LIFECYCLE_COLUMN_NAMES.name}</Th>
@@ -148,7 +135,7 @@ export const LifecycleTable: React.FunctionComponent<LifecycleTableProps> = ({ d
             </Th>
           </Tr>
         );
-      case 'lifecycle':
+      case 'rhel':
         return (
           <Tr key={LIFECYCLE_COLUMN_NAMES.name}>
             <Th sort={getSortParams(0)}>{LIFECYCLE_COLUMN_NAMES.name}</Th>
@@ -172,13 +159,10 @@ export const LifecycleTable: React.FunctionComponent<LifecycleTableProps> = ({ d
   };
 
   const renderData = (): React.ReactNode => {
-    if (!dataType) {
-      return;
-    }
-    switch (dataType) {
-      case 'appLifecycle':
-        return renderAppLifecycleData(data as AppLifecycleChanges[]);
-      case 'lifecycle':
+    switch (type) {
+      case 'streams':
+        return renderAppLifecycleData(data as Stream[]);
+      case 'rhel':
         return renderSystemLifecycleData(data as SystemLifecycleChanges[]);
       default:
         return;
@@ -186,14 +170,10 @@ export const LifecycleTable: React.FunctionComponent<LifecycleTableProps> = ({ d
   };
 
   const getAriaLabel = () => {
-    if (!dataType) {
-      return DEFAULT_ARIA_LABEL;
-    }
-
-    switch (dataType) {
-      case 'appLifecycle':
+    switch (type) {
+      case 'streams':
         return 'Table displaying RHEL 9 Application Streams Lifecycle information';
-      case 'lifecycle':
+      case 'rhel':
         return 'Table displaying Red Hat Enterprise Linux Lifecycle information';
       default:
         return DEFAULT_ARIA_LABEL;
