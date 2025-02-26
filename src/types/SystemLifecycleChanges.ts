222f97f074b84ace879756ee340adbd07068b7ff
Preston Watson <prwatson@redhat.com>
2025-02-26 14:03:03 -0500

    Update Lifecycle tab to accept backend changes (#42)
    * Update Lifecycle tab to accept backend changes
    
    * Remove changes to minor field

--- src/types/SystemLifecycleChanges.ts
+++ src/types/SystemLifecycleChanges.ts
@@ -1,10 +1,9 @@
 export type SystemLifecycleChanges = {
   name: string;
-  release: string;
   major: number;
   minor: number;
   release_date: string;
   retirement_date: string;
-  systems: number;
+  count: number;
   lifecycle_type: string;
 };
