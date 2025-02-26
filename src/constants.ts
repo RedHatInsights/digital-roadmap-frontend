3af16b828eb17538ff44393b1c41c59af9fde8f7
Preston Watson <prwatson@redhat.com>
2025-02-26 16:07:31 -0500

    Update api constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
-export const DR_API = '/api/roadmap/v1/relevant';
+export const DR_API = '/api/roadmap/v1';
 export const DR_RELEASE_NOTES = '/release-notes';
-export const DR_LIFECYCLE_SYSTEMS = '/lifecycle/rhel/';
-export const DR_LIFECYCLE_APPSTREAMS = '/lifecycle/app-streams/';
+export const DR_LIFECYCLE_SYSTEMS = '/relevant/lifecycle/rhel/';
+export const DR_LIFECYCLE_APPSTREAMS = '/relevant/lifecycle/app-streams/';
 export const DR_UPCOMING = '/upcoming-changes/';
 
 export const INVENTORY_API_ROOT = '/api/inventory/v1';
