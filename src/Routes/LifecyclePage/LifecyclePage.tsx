4a9ad814bb5a35e2bf2994012357a650bf39bc98
Adam Hosek <ahosek@redhat.com>
2025-03-12 16:08:19 +0100

    [RSPEED-755] Migrate tabs to pages
    The tabs needed to be migrated into separate pages accessible via dropdown menu.

---
+++ src/Routes/LifecyclePage/LifecyclePage.tsx
@@ -0,0 +1,41 @@
+import React, { Suspense, useEffect } from "react";
+import { Alert, Spinner } from "@patternfly/react-core";
+import {
+  PageHeader,
+  PageHeaderTitle,
+} from "@redhat-cloud-services/frontend-components/PageHeader";
+import LifecycleTab from "../../Components/Lifecycle/Lifecycle";
+import { useChrome } from "@redhat-cloud-services/frontend-components/useChrome";
+
+import "./LifecyclePage.scss";
+
+const LifecyclePage = () => {
+  const { appAction } = useChrome();
+  useEffect(() => {
+    appAction("digital-roadmap");
+  }, []);
+
+  return (
+    <React.Fragment>
+      <PageHeader>
+        <PageHeaderTitle title="Life Cycle" />
+      </PageHeader>
+      <Alert
+        id="lifecycle-warning"
+        variant="warning"
+        title="All future dates mentioned are close approximations, non definitive, and subject to change."
+        component="h2"
+      />
+      <section
+        className="pf-l-page__main-section pf-c-page__main-section"
+        id="lifecycle"
+      >
+        <Suspense fallback={<Spinner />}>
+          <LifecycleTab />
+        </Suspense>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default LifecyclePage;
---
+++ src/Routes/UpcomingPage/UpcomingPage.tsx
@@ -0,0 +1,41 @@
+import React, { Suspense, useEffect } from "react";
+import { Alert, Spinner } from "@patternfly/react-core";
+import {
+  PageHeader,
+  PageHeaderTitle,
+} from "@redhat-cloud-services/frontend-components/PageHeader";
+import UpcomingTab from "../../Components/Upcoming/Upcoming";
+import { useChrome } from "@redhat-cloud-services/frontend-components/useChrome";
+
+import "./UpcomingPage.scss";
+
+const UpcomingPage = () => {
+  const { appAction } = useChrome();
+  useEffect(() => {
+    appAction("digital-roadmap");
+  }, []);
+
+  return (
+    <React.Fragment>
+      <PageHeader>
+        <PageHeaderTitle title="Roadmap" />
+      </PageHeader>
+      <Alert
+        id="roadmap-warning"
+        variant="warning"
+        title="Upcoming features are subject to change. All future dates mentioned are close approximations, non definitive, and subject to change."
+        component="h2"
+      />
+      <section
+        className="pf-l-page__main-section pf-c-page__main-section"
+        id="roadmap"
+      >
+        <Suspense fallback={<Spinner />}>
+          <UpcomingTab />
+        </Suspense>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default UpcomingPage;
