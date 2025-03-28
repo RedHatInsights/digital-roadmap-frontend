568df28e173b8ad4dea4e898a299f23eae173213
Sam Doran <sdoran@redhat.com>
2025-03-28 18:35:50 -0400

    Set type to avoid runtime error

--- src/AppEntry.tsx
+++ src/AppEntry.tsx
@@ -7,7 +7,7 @@ import logger from 'redux-logger';
 const AppEntry = () => (
   <Provider
     store={init(
-      ...(process.env.NODE_ENV !== 'production' ? [logger] : [])
+      ...((process.env.NODE_ENV !== 'production' ? [logger] : []) as any)
     ).getStore()}
   >
     <App />
