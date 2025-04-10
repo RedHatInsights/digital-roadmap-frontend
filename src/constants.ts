ab09339228092437ecfe2e4710b288eb9e65d2b1
Sam Doran <sdoran@redhat.com>
2025-04-10 10:44:51 -0400

    Update API paths
    Trailing slashes were removed in the backend API.

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const DR_API = '/api/roadmap/v1';
 export const DR_RELEASE_NOTES = '/release-notes';
-export const DR_LIFECYCLE_SYSTEMS = '/relevant/lifecycle/rhel/';
-export const DR_LIFECYCLE_APPSTREAMS = '/relevant/lifecycle/app-streams/';
-export const DR_UPCOMING = '/upcoming-changes/';
+export const DR_LIFECYCLE_SYSTEMS = '/relevant/lifecycle/rhel';
+export const DR_LIFECYCLE_APPSTREAMS = '/relevant/lifecycle/app-streams';
+export const DR_UPCOMING = '/upcoming-changes';
 
 export const INVENTORY_API_ROOT = '/api/inventory/v1';
 export const INVENTORY_HOSTS_ROOT = '/hosts';
