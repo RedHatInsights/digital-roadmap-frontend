deb045ac938b1ccdf94ab7f0efa942538a164a6e
Preston Watson <prwatson@redhat.com>
2025-05-12 11:24:14 -0400

    Invert system sort

--- src/Components/Lifecycle/filteringUtils.ts
+++ src/Components/Lifecycle/filteringUtils.ts
@@ -126,15 +126,15 @@ export const filterChartDataByRelease = (data: Stream[] | SystemLifecycleChanges
 export const filterChartDataBySystems = (data: Stream[] | SystemLifecycleChanges[], dropdownValue: string) => {
   if (dropdownValue === DEFAULT_DROPDOWN_VALUE) {
     return (data as Stream[]).sort((a: Stream, b: Stream) => {
-      if (a.count > b.count) return -1;
-      if (a.count < b.count) return 1;
+      if (a.count < b.count) return -1;
+      if (a.count > b.count) return 1;
       return 0;
     });
   }
   if (dropdownValue === RHEL_8_STREAMS_DROPDOWN_VALUE) {
     return (data as Stream[]).sort((a: Stream, b: Stream) => {
-      if (a.count > b.count) return -1;
-      if (a.count < b.count) return 1;
+      if (a.count < b.count) return -1;
+      if (a.count > b.count) return 1;
       return 0;
     });
   }
