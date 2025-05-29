8c7c86404dd7c36689f64ef41519f33fac16378d
Adam Hosek <ahosek@redhat.com>
2025-05-29 11:57:17 +0200

    fix the infinite loop

--- src/Components/Lifecycle/Lifecycle.tsx
+++ src/Components/Lifecycle/Lifecycle.tsx
@@ -73,7 +73,6 @@ const LifecycleTab: React.FC<React.PropsWithChildren> = () => {
   // drop down menu
   const [lifecycleDropdownValue, setLifecycleDropdownValue] = React.useState<string>(DEFAULT_DROPDOWN_VALUE);
   const [chartSortByValue, setChartSortByValue] = React.useState<string>(DEFAULT_CHART_SORTBY_VALUE);
-  const [tableSortByValue, setTableSortByValue] = React.useState<string>('');
   const [filters, setFilters] = useState<ExtendedFilter>(DEFAULT_FILTERS);
   // Add state for view filter (all, installed-only, installed-and-related)
   const [selectedViewFilter, setSelectedViewFilter] = useState<string>('installed-only');
@@ -585,10 +584,6 @@ const LifecycleTab: React.FC<React.PropsWithChildren> = () => {
     fetchData(initialViewFilter);
   }, []);
 
-  useEffect(() => {
-    setChartSortByValue(tableSortByValue);
-  });
-
   // Update resetDataFiltering to use the properly filtered app data
   const resetDataFiltering = () => {
     let currentData: Stream[] | SystemLifecycleChanges[] = [];
@@ -832,7 +827,7 @@ const LifecycleTab: React.FC<React.PropsWithChildren> = () => {
           data={filteredTableData}
           viewFilter={selectedViewFilter}
           chartSortByValue={chartSortByValue}
-          setTableSortByValue={setTableSortByValue}
+          setTableSortByValue={setChartSortByValue}
         />
       </>
     );
