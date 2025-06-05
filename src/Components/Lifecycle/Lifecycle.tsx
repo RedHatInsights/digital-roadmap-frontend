50286e34b275f169e82adc94c1dc86c13a172ae9
Adam Hosek <ahosek@redhat.com>
2025-06-05 19:23:21 +0200

    Fix the sync after dropdown change
    Sync table with chart wasn't working when Lifecycle dropdown was
    changed. Now it's synchronized.

--- src/Components/Lifecycle/Lifecycle.tsx
+++ src/Components/Lifecycle/Lifecycle.tsx
@@ -750,6 +750,7 @@ const LifecycleTab: React.FC<React.PropsWithChildren> = () => {
           viewFilter={selectedViewFilter}
           chartSortByValue={chartSortByValue}
           updateChartSortValue={updateChartSortValue}
+          lifecycleDropdownValue={lifecycleDropdownValue}
         />
       </>
     );
