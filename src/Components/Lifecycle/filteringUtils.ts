a6b3407c990fa199a095b06ad0bd25aa616ef6c2
Adam Hosek <ahosek@redhat.com>
2025-06-06 13:04:20 +0200

    Add option for sorting control

--- src/Components/Lifecycle/filteringUtils.ts
+++ src/Components/Lifecycle/filteringUtils.ts
@@ -7,20 +7,34 @@ export const RHEL_8_STREAMS_DROPDOWN_VALUE = 'RHEL 8 Application Streams';
 export const RHEL_10_STREAMS_DROPDOWN_VALUE = 'RHEL 10 Application Streams';
 export const RHEL_SYSTEMS_DROPDOWN_VALUE = 'Red Hat Enterprise Linux';
 
-export const filterChartDataByName = (data: Stream[] | SystemLifecycleChanges[], dropdownValue: string) => {
+export const filterChartDataByName = (
+  data: Stream[] | SystemLifecycleChanges[],
+  dropdownValue: string,
+  order: string = 'asc'
+) => {
+  // Needed to be able to switch order from ascending to descending
+  const order_control = order === 'asc' ? 1 : -1;
+
   if (
     [DEFAULT_DROPDOWN_VALUE, RHEL_8_STREAMS_DROPDOWN_VALUE, RHEL_10_STREAMS_DROPDOWN_VALUE].includes(dropdownValue)
   ) {
     return (data as Stream[]).sort((a: Stream, b: Stream) => {
-      return b.display_name.localeCompare(a.display_name, undefined, { numeric: true });
+      return order_control * b.display_name.localeCompare(a.display_name, undefined, { numeric: true });
     });
   }
   return (data as SystemLifecycleChanges[]).sort((a, b) => {
-    return b.display_name.localeCompare(a.display_name, undefined, { numeric: true });
+    return order_control * b.display_name.localeCompare(a.display_name, undefined, { numeric: true });
   });
 };
 
-export const filterChartDataByReleaseDate = (data: Stream[] | SystemLifecycleChanges[], dropdownValue: string) => {
+export const filterChartDataByReleaseDate = (
+  data: Stream[] | SystemLifecycleChanges[],
+  dropdownValue: string,
+  order: string = 'asc'
+) => {
+  // Needed to be able to switch order from ascending to descending
+  const order_control = order === 'asc' ? 1 : -1;
+
   if (
     [DEFAULT_DROPDOWN_VALUE, RHEL_8_STREAMS_DROPDOWN_VALUE, RHEL_10_STREAMS_DROPDOWN_VALUE].includes(dropdownValue)
   ) {
@@ -28,8 +42,8 @@ export const filterChartDataByReleaseDate = (data: Stream[] | SystemLifecycleCha
       const aStart = new Date(a.start_date);
       const bStart = new Date(b.start_date);
 
-      if (aStart.getTime() > bStart.getTime()) return -1;
-      if (aStart.getTime() < bStart.getTime()) return 1;
+      if (aStart.getTime() > bStart.getTime()) return order_control * -1;
+      if (aStart.getTime() < bStart.getTime()) return order_control * 1;
       return 0;
     });
   }
@@ -37,65 +51,83 @@ export const filterChartDataByReleaseDate = (data: Stream[] | SystemLifecycleCha
     const aStart = new Date(a.start_date);
     const bStart = new Date(b.start_date);
 
-    if (aStart.getTime() > bStart.getTime()) return -1;
-    if (aStart.getTime() < bStart.getTime()) return 1;
+    if (aStart.getTime() > bStart.getTime()) return order_control * -1;
+    if (aStart.getTime() < bStart.getTime()) return order_control * 1;
     return 0;
   });
 };
 
 export const filterChartDataByRetirementDate = (
   data: Stream[] | SystemLifecycleChanges[],
-  dropdownValue: string
+  dropdownValue: string,
+  order: string = 'asc'
 ) => {
+  // Needed to be able to switch order from ascending to descending
+  const order_control = order === 'asc' ? 1 : -1;
+
   if (
     [DEFAULT_DROPDOWN_VALUE, RHEL_8_STREAMS_DROPDOWN_VALUE, RHEL_10_STREAMS_DROPDOWN_VALUE].includes(dropdownValue)
   ) {
     return (data as Stream[]).sort((a: Stream, b: Stream) => {
-      return new Date(b.end_date).getTime() - new Date(a.end_date).getTime();
+      return order_control * (new Date(b.end_date).getTime() - new Date(a.end_date).getTime());
     });
   }
   return (data as SystemLifecycleChanges[]).sort((a, b) => {
     const aEnd = new Date(a.end_date);
     const bEnd = new Date(b.end_date);
-    if (aEnd.getTime() > bEnd.getTime()) return -1;
-    if (aEnd.getTime() < bEnd.getTime()) return 1;
+    if (aEnd.getTime() > bEnd.getTime()) return order_control * -1;
+    if (aEnd.getTime() < bEnd.getTime()) return order_control * 1;
     return 0;
   });
 };
 
-export const filterChartDataByRelease = (data: Stream[] | SystemLifecycleChanges[], dropdownValue: string) => {
+export const filterChartDataByRelease = (
+  data: Stream[] | SystemLifecycleChanges[],
+  dropdownValue: string,
+  order: string = 'asc'
+) => {
+  // Needed to be able to switch order from ascending to descending
+  const order_control = order === 'asc' ? 1 : -1;
+
   if (
     [DEFAULT_DROPDOWN_VALUE, RHEL_8_STREAMS_DROPDOWN_VALUE, RHEL_10_STREAMS_DROPDOWN_VALUE].includes(dropdownValue)
   ) {
     return (data as Stream[]).sort((a: Stream, b: Stream) => {
       // Compare major versions first
-      if (a.os_major > b.os_major) return -1;
-      if (a.os_major < b.os_major) return 1;
+      if (a.os_major > b.os_major) return order_control * -1;
+      if (a.os_major < b.os_major) return order_control * 1;
       // If major versions are equal, compare minor versions
-      if (a.os_minor > b.os_minor) return -1;
-      if (a.os_minor < b.os_minor) return 1;
+      if (a.os_minor > b.os_minor) return order_control * -1;
+      if (a.os_minor < b.os_minor) return order_control * 1;
       return 0;
     });
   }
   // Using full RHEL name comparison instead of major and minor so we can also take into account the lifecycle type
   return (data as SystemLifecycleChanges[]).sort((a, b) => {
-    return b.display_name.localeCompare(a.display_name, undefined, { numeric: true });
+    return order_control * b.display_name.localeCompare(a.display_name, undefined, { numeric: true });
   });
 };
 
-export const filterChartDataBySystems = (data: Stream[] | SystemLifecycleChanges[], dropdownValue: string) => {
+export const filterChartDataBySystems = (
+  data: Stream[] | SystemLifecycleChanges[],
+  dropdownValue: string,
+  order: string = 'desc'
+) => {
+  // Needed to be able to switch order from ascending to descending
+  const order_control = order === 'asc' ? 1 : -1;
+
   if (
     [DEFAULT_DROPDOWN_VALUE, RHEL_8_STREAMS_DROPDOWN_VALUE, RHEL_10_STREAMS_DROPDOWN_VALUE].includes(dropdownValue)
   ) {
     return (data as Stream[]).sort((a: Stream, b: Stream) => {
-      if (a.count < b.count) return -1;
-      if (a.count > b.count) return 1;
+      if (a.count < b.count) return order_control * -1;
+      if (a.count > b.count) return order_control * 1;
       return 0;
     });
   }
   return (data as SystemLifecycleChanges[]).sort((a, b) => {
-    if (a.count < b.count) return -1;
-    if (a.count > b.count) return 1;
+    if (a.count < b.count) return order_control * -1;
+    if (a.count > b.count) return order_control * 1;
     return 0;
   });
 };
