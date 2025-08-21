c6d0c7b9b310f49e4e737a0aeb565d288742b14a
Preston Watson <prwatson@redhat.com>
2025-08-21 08:54:14 -0400

    Sanity

--- src/Components/LifecycleTable/LifecycleTable.test.tsx
+++ src/Components/LifecycleTable/LifecycleTable.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen, waitFor, act } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { LifecycleTable } from './LifecycleTable';
