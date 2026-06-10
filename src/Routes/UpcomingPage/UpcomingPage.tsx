89eb1462707f9741b563107b9cf691607cfdeadf
Preston Watson <prwatson@redhat.com>
2026-06-10 10:23:16 -0400

    [RSPEED-3164] Add links to the red hat docs for the about section in Planning (#289)
    * Add links to the red hat docs for the about section in Planning
    
    * Add tests
    
    * Test files

--- src/Routes/UpcomingPage/UpcomingPage.tsx
+++ src/Routes/UpcomingPage/UpcomingPage.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, useEffect } from 'react';
-import { Alert, Button, Popover, Spinner } from '@patternfly/react-core';
+import { Alert, Button, Content, Popover, Spinner } from '@patternfly/react-core';
 import { OutlinedQuestionCircleIcon } from '@patternfly/react-icons';
 import { PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components/PageHeader';
 import UpcomingTab from '../../Components/Upcoming/Upcoming';
@@ -14,14 +14,31 @@ const UpcomingPage = () => {
   }, []);
 
   const popoverContent = (
-    <div>
-      View the latest updates on upcoming Red Hat Enterprise Linux features, tailored to your systems. Add systems
-      to{' '}
-      <a href="https://console.redhat.com/insights/inventory" target="_blank" rel="noopener noreferrer">
-        Inventory
-      </a>{' '}
-      to view only upcoming features relevant to your organization.
-    </div>
+    <Content>
+      <p>
+        View the latest updates on upcoming Red Hat Enterprise Linux features, tailored to your systems. Add
+        systems to{' '}
+        <a href="https://console.redhat.com/insights/inventory" target="_blank" rel="noopener noreferrer">
+          Inventory
+        </a>{' '}
+        to view only upcoming features relevant to your organization.
+      </p>
+      <p>
+        Learn more about{' '}
+        <a
+          href={
+            'https://docs.redhat.com/en/documentation/red_hat_lightspeed/1-latest/html/' +
+            'dynamically_creating_a_digital_roadmap_to_manage_rhel_systems/' +
+            'using-the-digital-roadmap-dashboard#upcoming-features'
+          }
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Using Red Hat Lightspeed for RHEL planning Dashboard
+        </a>
+        .
+      </p>
+    </Content>
   );
 
   return (
